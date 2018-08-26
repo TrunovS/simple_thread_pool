b0f939ed3c72bc227febcc33169cfd217c8121a6
Trunov Sergey <trunovsergey@list.ru>
2018-08-26 23:34:38 +0300

    Поготовка к многопоточной обработке вызовов.

--- src/lib.rs
+++ src/lib.rs
@@ -7,24 +7,33 @@ mod tests {
 }
 
 use std::thread;
+use std::sync::mpsc;
+
+type Job = Box<FnOnce() + Send + 'static>;
 
 struct Worker {
     id: usize,
     handle: thread::JoinHandle<()>,
-};
+}
 
 impl Worker {
-    fn new(id: usize) -> Worker {
+    fn new(id: usize, receiver: mpsc::Receiver<Job>) -> Worker {
+
+        let handle = thread::spawn(|| {
+            receiver;
+            });
+
         Worker {
             id,
-            thread::spawn(|| { }),
+            handle,
         }
     }
-};
+}
 
 pub struct ThreadPool {
     workers: Vec<Worker>,
-};
+    sender: mpsc::Sender<Job>,
+}
 
 
 impl ThreadPool {
@@ -38,21 +47,24 @@ impl ThreadPool {
     pub fn new(size: usize) -> ThreadPool
     {
         assert!(size>0);
+
+        let (sender, receiver) = mpsc::channel();
         let mut workers = Vec::with_capacity(size);
 
         for wid in 0..size {
-            workers.push(Worker::new(wid))
+            workers.push(Worker::new(wid, receiver))
         }
 
         ThreadPool {
-            workers
+            workers,
+            sender
         }
     }
 
-    pub fn start<F>(&self, f: F)
+    pub fn execute<F>(&self, f: F)
         where
         F: FnOnce() + Send + 'static
     {
 
     }
-};
+}
